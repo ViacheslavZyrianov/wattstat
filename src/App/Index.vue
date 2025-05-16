6880c500c1622e2b55f15751675ec94cc6e1f091
viacheslavzyrianov <viacheslav@metricsmatter.com>
2025-05-17 00:20:02 +0300

    feat: implement dynamic layout switching based on route metadata

--- src/App/Index.vue
+++ src/App/Index.vue
@@ -1,30 +1,29 @@
 <script setup lang="ts">
-import TabBar from '@/App/components/TabBar/Index.vue'
-import DialogConfirm from '@/App/components/DialogConfirm/Index.vue'
-import { useAuthStore } from '@/store/auth'
-import { useRoute } from 'vue-router'
 import { computed, ComputedRef } from 'vue'
+import { useRoute } from 'vue-router'
+import DefaultLayout from '@/layouts/DefaultLayout.vue'
+import AuthLayout from '@/layouts/AuthLayout.vue'
+import FullPageLayout from '@/layouts/FullPageLayout.vue'
 import { useUIStore } from '@/store/ui'
-import ActionSheetAddEditReading from '@/views/Readings/ActionSheetAddEditReading.vue'
 
 const route = useRoute()
-const authStore = useAuthStore()
 const uiStore = useUIStore()
 
-const title: ComputedRef<string> = computed(() => (route.name as string) || '')
-
-const isTitleVisible: ComputedRef<boolean> = computed(() =>
-  Boolean(authStore.getIsAuthenticated && title.value),
-)
+const layout: ComputedRef<unknown> = computed(() => {
+  switch (route.meta.layout) {
+    case 'FullPageLayout':
+      return FullPageLayout
+    case 'AuthLayout':
+      return AuthLayout
+    default:
+      return DefaultLayout
+  }
+})
 </script>
 
 <template>
   <van-config-provider :theme="uiStore.getTheme" />
-  <van-nav-bar v-if="isTitleVisible" :title="title" />
-  <main>
+  <component :is="layout">
     <router-view />
-  </main>
-  <tab-bar v-if="authStore.getIsAuthenticated" />
-  <dialog-confirm />
-  <action-sheet-add-edit-reading />
+  </component>
 </template>
