821ff55b962e2bd14d195c38904f3b8c1b17a900
viacheslavzyrianov <viacheslav@metricsmatter.com>
2025-05-17 00:36:45 +0300

    feat: add PWA install prompt functionality to Index.vue

--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -1,7 +1,30 @@
-<script setup lang="ts"></script>
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const deferredPrompt = ref(null)
+
+onMounted(() => {
+  window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault()
+    deferredPrompt.value = e
+  })
+})
+
+const installApp = async () => {
+  if (!deferredPrompt.value) return
+  deferredPrompt.value.prompt()
+  const choiceResult = await deferredPrompt.value.userChoice
+  if (choiceResult.outcome === 'accepted') {
+    console.log('User accepted the install prompt')
+  } else {
+    console.log('User dismissed the install prompt')
+  }
+  deferredPrompt.value = null
+}
+</script>
 
 <template>
-  <h1>HOME</h1>
+  <van-button v-if="deferredPrompt" @click="installApp">
+    Install WattStat
+  </van-button>
 </template>
-
-<style scoped lang="scss"></style>
