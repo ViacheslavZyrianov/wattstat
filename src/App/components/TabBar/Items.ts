73c72379dc7e785740a1dd3d88236512e8d78e35
viacheslavzyrianov <viacheslav@metricsmatter.com>
2025-05-23 23:14:07 +0300

    fix: update tabBarItems to set path as an empty string instead of null

--- src/App/components/TabBar/Items.ts
+++ src/App/components/TabBar/Items.ts
@@ -3,7 +3,7 @@ import { TabBarItem } from '@/App/components/TabBar/types'
 const tabBarItems: TabBarItem[] = [
   { icon: 'list-switching', name: 'Dashboard', path: '/dashboard' },
   { icon: 'user-o', name: 'Profile', path: '/profile' },
-  { icon: 'add', name: '', path: null, color: '#07c160' },
+  { icon: 'add', name: '', path: '', color: '#07c160' },
   { icon: 'orders-o', name: 'Readings', path: '/readings' },
   { icon: 'setting-o', name: 'Settings', path: '/settings' },
 ]
