74ec533bbca842a37b5e8282c10446d0616857fa
viacheslavzyrianov <viacheslav@metricsmatter.com>
2025-05-30 21:13:46 +0300

    feat: enhance Google authentication handling in Axios interceptor

--- src/axios.ts
+++ src/axios.ts
@@ -1,6 +1,7 @@
 // axios.js
 import axios from 'axios'
 import { showNotify } from 'vant'
+import { useGoogleAuthStore } from '@/store/auth/google'
 
 // Create an Axios instance
 const axiosInstance = axios.create({
@@ -16,9 +17,10 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
   (config) => {
     // Add auth token if available
-    const token = JSON.parse(localStorage.getItem('googleAuth')).token
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`
+    const googleAuth = localStorage.getItem('googleAuth')
+    const tokenParsed = googleAuth ? JSON.parse(googleAuth).token : null
+    if (tokenParsed) {
+      config.headers.Authorization = `Bearer ${tokenParsed}`
     }
     return config
   },
@@ -32,7 +34,10 @@ axiosInstance.interceptors.response.use(
     const apiUrl = error.config?.url || 'Unknown URL'
     const noErrorApiUrls = ['/auth/status']
 
-    if (!noErrorApiUrls.includes(apiUrl)) {
+    if (error.status === 403) {
+      const googleAuthStore = useGoogleAuthStore()
+      googleAuthStore.logout()
+    } else if (!noErrorApiUrls.includes(apiUrl)) {
       showNotify({
         message: `ERROR: ${error.message}\nURL: ${apiUrl}`,
         background: '#ee0a24',
