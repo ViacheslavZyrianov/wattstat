1759ea3056c1887864e9cbbe03b4798a14b4e2a8
viacheslavzyrianov <viacheslav@metricsmatter.com>
2025-07-12 19:07:30 +0300

    feat: enhance Index.vue layout with app description and footer navigation

--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -24,5 +24,41 @@ const installApp = async () => {
 </script>
 
 <template>
-  <v-btn v-if="deferredPrompt" @click="installApp"> Install WattStat </v-btn>
+  <v-container>
+    <v-card>
+      <h4 class="text-h4 font-weight-bold mb-4">
+        Watt's Up?<br />
+        It's WattStat!
+      </h4>
+      <p class="mb-4">
+        Whether you’re a data nerd or just curious, WattStat helps you keep your
+        electricity in check. Simple logging, powerful stats.
+      </p>
+      <p class="mb-4">Add it to your phone for quick, app-like access!</p>
+      <v-btn
+        v-if="deferredPrompt"
+        color="primary"
+        prepend-icon="mdi-lightning-bolt"
+        @click="installApp"
+      >
+        Install WattStat
+      </v-btn>
+    </v-card>
+  </v-container>
+  <v-footer class="position-fixed bottom-0 right-0 left-0 flex justify-center">
+    <v-btn
+      href="/privacy-policy"
+      variant="text"
+      class="text-decoration-underline"
+    >
+      Privacy Policy
+    </v-btn>
+    <v-btn
+      href="/terms-of-service"
+      variant="text"
+      class="text-decoration-underline"
+    >
+      Terms Of Service
+    </v-btn>
+  </v-footer>
 </template>
